03d4b5821c9dc13ed7b29af3e3d9df27de13bcab
Saurabh <53432550+gsaurabh2311@users.noreply.github.com>
2023-02-19 17:01:31 +0530

    Add files via upload

---
+++ LRUCache.js
@@ -0,0 +1,43 @@
+class LruCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.cache = [];
+    }
+
+    isCacheFull() {
+        return this.cache.length === this.capacity;
+    }
+
+    getItem(item) {
+        return this.cache.indexOf(item);
+    }
+    
+    setItem(item) {
+        const itemIndex = this.getItem(item);
+        if(itemIndex === -1) {
+            if(this.isCacheFull()) {
+                this.cache.pop();
+                this.cache.unshift(item);
+            } else {
+                this.cache.unshift(item);
+            }
+        }
+        if(itemIndex !== -1) {
+            this.cache.splice(itemIndex,1);
+            this.cache.unshift(item);
+        }
+        console.log("Cache -->", this.cache);
+    }
+}
+
+let cache = new LruCache(4);
+cache.setItem(10);
+cache.setItem(20);
+cache.setItem(10);
+cache.setItem(30);
+cache.setItem(40);
+cache.setItem(50);
+cache.setItem(30);
+cache.setItem(40);
+cache.setItem(60);
+cache.setItem(30);
